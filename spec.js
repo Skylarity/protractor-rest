7f4c09772f7034dcf8ac9460ad11c4bca0c1724c
Skyler Rexroad <skyrex1095@gmail.com>
2016-02-05 10:48:48 -0700

    Required execute-request.js

--- spec.js
+++ spec.js
@@ -1,3 +1,5 @@
+require("./execute-request.js")
+
 describe("Misquote of the Day", function() {
     it("should have some misquotes", function() {
         executeRequest("GET", "https://bootcamp-coders.cnm.edu/~dmcdonald21/misquote-of-the-day/public_html/api/misquote/");
