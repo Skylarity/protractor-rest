19c60b55a0433308849da0837346182692430be2
Skyler Rexroad <skyrex1095@gmail.com>
2016-02-05 10:46:12 -0700

    Import of existing code

---
+++ spec.js
@@ -0,0 +1,5 @@
+describe("Misquote of the Day", function() {
+    it("should have some misquotes", function() {
+        executeRequest("GET", "https://bootcamp-coders.cnm.edu/~dmcdonald21/misquote-of-the-day/public_html/api/misquote/");
+    });
+});
