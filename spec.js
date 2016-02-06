b6173bdbc9ae7d7ba81b916647df8fb38feaf8f8
Dylan McDonald <dmcdonald21@cnm.edu>
2016-02-06 15:28:28 -0700

    deleted cloaked characters

--- spec.js
+++ spec.js
@@ -1,35 +1,6 @@
-// require("./execute-request");
-// console.log(typeof execute-request.executeRequest);
+require("./execute-request");
+console.log(typeof execute-request.executeRequest);
 
-var request = require("request");
-// var executeRequest = function(method, url, data) {
-// 	var defer = protractor.promise.defer();
-//
-// 	var requestOptions = {uri: url, method: method, json: true};
-// 	if(data !== undefined && (method === "POST" || method === "PUT")) {
-// 		requestOptions.body = data;
-// 	}
-// 	​
-// 	// method can be ‘GET’, ‘POST’ or ‘PUT’
-// 	request(requestOptions, function(error, response, body) {
-// 	​
-// 		if (error || response.statusCode >= 400) {
-// 			defer.reject({
-// 				error : error,
-// 				message : response
-// 			});
-// 		} else {
-// 			defer.fulfill(body);
-// 		}
-// 	});
-//
-// 	// Return a promise so the caller can wait on it for the request to complete
-// 	return defer.promise;
-// };
-
-var executeRequest = function() {
-	return(true);
-};
 
 describe("Misquote of the Day", function() {
 	it("should have some misquotes", function() {
