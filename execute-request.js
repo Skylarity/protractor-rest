b5a076f1ccbf17e82938b31f436f9ee6a78957e5
Dylan McDonald <dmcdonald21@cnm.edu>
2016-02-06 15:06:16 -0700

    checkpoint commit for @srexroad

--- execute-request.js
+++ execute-request.js
@@ -1,25 +1,31 @@
-var request = require("request");
-var executeRequest = function(method, url, data) {
-	var defer = protractor.promise.defer();
+// module.exports = {
+// 	var executeRequest = function(method, url, data) {
+// 		var request = require("request");
+// 		var defer = protractor.promise.defer();
+//
+// 		var requestOptions = {uri: url, method: method, json: true};
+// 		if(data !== undefined && (method === "POST" || method === "PUT")) {
+// 			requestOptions.body = data;
+// 		}
+// 		​
+// 		// method can be ‘GET’, ‘POST’ or ‘PUT’
+// 		request(requestOptions, function(error, response, body) {
+// 		​
+// 			if (error || response.statusCode >= 400) {
+// 				defer.reject({
+// 					error : error,
+// 					message : response
+// 				});
+// 			} else {
+// 				defer.fulfill(body);
+// 			}
+// 		});
+//
+// 		// Return a promise so the caller can wait on it for the request to complete
+// 		return defer.promise;
+// 	};
+// }
 
-	var requestOptions = {uri: url, method: method, json: true};
-	if(data !== undefined && (method === "POST" || method === "PUT")) {
-		requestOptions.body = data;
-	}
-	​
-	// method can be ‘GET’, ‘POST’ or ‘PUT’
-	request(requestOptions, function(error, response, body) {
-	​
-		if (error || response.statusCode >= 400) {
-			defer.reject({
-				error : error,
-				message : response
-			});
-		} else {
-			defer.fulfill(body);
-		}
-	});
-
-	// Return a promise so the caller can wait on it for the request to complete
-	return defer.promise;
+var executeRequest = function() {
+	return(true);
 };
--- spec.js
+++ spec.js
@@ -1,7 +1,41 @@
-require("./execute-request.js")
+// require("./execute-request");
+// console.log(typeof execute-request.executeRequest);
+
+var request = require("request");
+// var executeRequest = function(method, url, data) {
+// 	var defer = protractor.promise.defer();
+//
+// 	var requestOptions = {uri: url, method: method, json: true};
+// 	if(data !== undefined && (method === "POST" || method === "PUT")) {
+// 		requestOptions.body = data;
+// 	}
+// 	​
+// 	// method can be ‘GET’, ‘POST’ or ‘PUT’
+// 	request(requestOptions, function(error, response, body) {
+// 	​
+// 		if (error || response.statusCode >= 400) {
+// 			defer.reject({
+// 				error : error,
+// 				message : response
+// 			});
+// 		} else {
+// 			defer.fulfill(body);
+// 		}
+// 	});
+//
+// 	// Return a promise so the caller can wait on it for the request to complete
+// 	return defer.promise;
+// };
+
+var executeRequest = function() {
+	return(true);
+};
 
 describe("Misquote of the Day", function() {
 	it("should have some misquotes", function() {
-			executeRequest("GET", "https://bootcamp-coders.cnm.edu/~dmcdonald21/misquote-of-the-day/public_html/api/misquote/");
+			executeRequest("GET", "https://bootcamp-coders.cnm.edu/~dmcdonald21/misquote-of-the-day/public_html/api/misquote/")
+			.then(function(data) {
+				console.log("yay! teh data!");
+			});
 		});
 });
