4d17a98c7f2f63c49c3e725e485840eb7d0727c1
Dylan McDonald <dmcdonald21@cnm.edu>
2016-02-06 14:40:20 -0700

    Replaced spaces with tabs

--- execute-request.js
+++ execute-request.js
@@ -1,25 +1,25 @@
 var request = require("request");
 var executeRequest = function(method, url, data) {
-    var defer = protractor.promise.defer();
+	var defer = protractor.promise.defer();
 
-    var requestOptions = {uri: url, method: method, json: true};
-    if(data !== undefined && (method === "POST" || method === "PUT")) {
-        requestOptions.body = data;
-    }
-​
-    // method can be ‘GET’, ‘POST’ or ‘PUT’
-    request(requestOptions, function(error, response, body) {
-​
-        if (error || response.statusCode >= 400) {
-            defer.reject({
-                error : error,
-                message : response
-            });
-        } else {
-            defer.fulfill(body);
-        }
-    });
+	var requestOptions = {uri: url, method: method, json: true};
+	if(data !== undefined && (method === "POST" || method === "PUT")) {
+		requestOptions.body = data;
+	}
+	​
+	// method can be ‘GET’, ‘POST’ or ‘PUT’
+	request(requestOptions, function(error, response, body) {
+	​
+		if (error || response.statusCode >= 400) {
+			defer.reject({
+				error : error,
+				message : response
+			});
+		} else {
+			defer.fulfill(body);
+		}
+	});
 
-    // Return a promise so the caller can wait on it for the request to complete
-    return defer.promise;
+	// Return a promise so the caller can wait on it for the request to complete
+	return defer.promise;
 };
--- spec.js
+++ spec.js
@@ -1,7 +1,7 @@
 require("./execute-request.js")
 
 describe("Misquote of the Day", function() {
-    it("should have some misquotes", function() {
-        executeRequest("GET", "https://bootcamp-coders.cnm.edu/~dmcdonald21/misquote-of-the-day/public_html/api/misquote/");
-    });
+	it("should have some misquotes", function() {
+			executeRequest("GET", "https://bootcamp-coders.cnm.edu/~dmcdonald21/misquote-of-the-day/public_html/api/misquote/");
+		});
 });
